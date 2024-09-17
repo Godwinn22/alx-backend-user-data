4c35fe6a0bc52992bae92c0528ca27e27f9d9be8
godwinn22 <godwinohazulike@gmail.com>
2024-09-17 12:49:53 +0100

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,9 +14,21 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required based on the path.
-        For now, it returns False.
+        Returns:
+            - True if the path is None or not in the excluded paths.
+            - False if the path is in the excluded paths.
         """
-        return False
+        if path is None:
+            return True
+        # Ensure both path and excluded paths are slash-tolerant
+        if path[-1] != '/':
+            path += '/'
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the value of the Authorization header\
